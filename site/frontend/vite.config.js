7942688798bf0d059759793b471a1fa16de1500c
ApurvChaturOfficial <apurvchaturoffiial@gmail.com>
2024-01-10 11:59:09 +0530

    Hey

--- site/frontend/vite.config.js
+++ site/frontend/vite.config.js
@@ -8,16 +8,26 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      manifest:  {
-        "name": "Pinwheel Organisation",
-        "icons": [
+      manifest: {
+        "registerType": 'autoUpdate',
+        "name":"Pinwheel Organisation",
+        "short_name":"Pinwheel",
+        "display": "standalone",
+        "background_color": "#241468",
+        "theme_color": "#241468",
+        "icons":[
           {
             src: "Business/Pinwheel/favicon_io/android-chrome-512x512.png",
             sizes: "512x512",
             type: "image/png",
-          }
+          },
+          {
+            src: "Business/Pinwheel/favicon_io/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
         ],
-      } 
+      }
     })
   ],
  
